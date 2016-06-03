e236e76423ce7fbd0d2843783aac67c1e01b8b32
Peter Camp <Pete.f.Camp@gmail.com>
2016-06-02 20:39:29 -0500

    added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.create!(username:"Pete", lastname: "Camp", email: "Pete.F.Camp@gmail.com", password: "pete", password_confirmation: "pete")
+User.create!(username:"StephenCurry", lastname: "Curry", email: "stephencurry@warriors.com", password: "warriors", password_confirmation: "warriors")
+
